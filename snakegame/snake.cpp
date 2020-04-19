4966abcc85e22df9dd02de51c127e8eeb7356b43
Hamza <hamza40nazir@gmail.com>
2020-04-19 12:37:12 +0500

    final commit

--- snakegame/snake.cpp
+++ snakegame/snake.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<conio.h>
 #include<windows.h>
+#include<dos.h>
+#include<time.h>
+
+
+#define MAXSNAKESIZE 100
 
 using namespace std;
 
@@ -73,14 +78,121 @@ public:
 	{
 		cout << "(" << x << "," << y << ")";
 	}
+	void CopyPosition(Point *p)
+	{
+		p->x = x;
+		p->y = y;
+	}
 };
+class Snake {
+private:
+	Point* cell[MAXSNAKESIZE];
+	int size;
+	int direction;
+	Point fruit;
+public:
 
+	Snake()
+	{
+		size = 1;
+		cell[0] = new Point(25, 25);
+		for (int i = 1; i < MAXSNAKESIZE; i++)
+		{
+			cell[i] = NULL;
+		}
+	}
+	
+	fruit.setPoint(srand() % 50, srand() % 50);
+	void AddCell(int x, int y)
+	{
+		cell[size++] = new Point(x, y);
+	}
+
+	void TurnUp() {
+		direction = 'w';
+	}
+	void TurnDown() {
+		direction = 's';
+	}
+	void TurnLeft() {
+		direction = 'a';
+	}
+	void TurnRight() {
+		direction = 'd';
+	}
+	void Move()
+	{
+		system("cls");
+		for (int i = size - 1; i > ; i--)
+		{
+			cell[i-1]->CopyPosition(cell[i]); // copy the snake body ahead
+		}
+
+		switch (direction) {
+		case 'w':
+			cell[0]->MoveUp();
+			break;
+		case 's':
+			cell[0]->MoveDown();
+			break;
+		case'a':
+			cell[0]->MoveLeft();
+			break;
+		case'd':
+			cell[0]->MoveRight();
+			break;
+		}
+
+		if (fruit.GetX == cell[0]->GetX && fruit.GetY == cell[0]->GetY)
+		{
+			AddCell(0, 0);
+			fruit.setPoint(srand() % 50, srand() % 50);
+		}
+
+		for (int i = 0; i < size; i++)
+		{
+			cell[i]->Draw();
+		}
+		fruit.Draw();
+		Sleep(100);
+	}
+};
 
 int main()
 {
-	Point p(5, 10);
-	p.Draw();
 
+	srand((unsigned)time(NULL));
+
+
+	Snake snake;
+	char op = 'l';
+
+	while (op != 'e')
+	{
+		if (_kbhit()) {
+			op = _getch();
+		}
+
+		switch (op) {
+		case'w':
+		case'W':
+			snake.TurnUp();
+			break;
+		case's':
+		case'S':
+			snake.TurnDown();
+			break;
+		case'a':
+		case'A':
+			snake.TurnLeft();
+			break;
+		case'd':
+		case'D':
+			snake.TurnDown();
+			break;
+		}
+		snake.Move();
+	}
 
 
 	system("pause");
